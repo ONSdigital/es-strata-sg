14138119068a75b97c7e47f6fad23c181b943122
Michael Candlin <mike@candlin.net>
2019-10-28 12:19:13 +0000

    Update strata_period_wrangler.py

--- strata_period_wrangler.py
+++ strata_period_wrangler.py
@@ -6,6 +6,7 @@ import random
 import boto3
 import marshmallow
 from botocore.exceptions import ClientError, IncompleteReadError
+from esawsfunctions import funk
 
 
 class EnvironSchema(marshmallow.Schema):
@@ -18,10 +19,7 @@ class EnvironSchema(marshmallow.Schema):
     checkpoint = marshmallow.fields.Str(required=True)
     method_name = marshmallow.fields.Str(required=True)
     sqs_message_group_id = marshmallow.fields.Str(required=True)
-
-
-class NoDataInQueueError(Exception):
-    pass
+    incoming_message_group = marshmallow.fields.Str(required=True)
 
 
 def lambda_handler(event, context):
@@ -58,46 +56,34 @@ def lambda_handler(event, context):
         queue_url = config["queue_url"]
         method_name = config["method_name"]
         sqs_message_group_id = config["sqs_message_group_id"]
-
-        # Reads in Data from SQS Queue
-        response = get_sqs_message(queue_url)
-        if "Messages" not in response:
-            raise NoDataInQueueError("No Messages in queue")
-
-        message = response["Messages"][0]
-        message_json = json.loads(message["Body"])
-        receipt_handle = message["ReceiptHandle"]
+        incoming_message_group = config["incoming_message_group"]
+        message_json, receipt_handle = funk.get_data(queue_url, bucket_name, "enrichment_out.json",incoming_message_group)
 
         logger.info("Successfully retrieved data from sqs")
 
         returned_data = var_lambda.invoke(
-            FunctionName=method_name, Payload=json.dumps(message_json)
+            FunctionName=method_name, Payload=message_json
         )
 
         json_response = returned_data.get("Payload").read().decode("UTF-8")
 
         logger.info("Successfully invoked lambda")
 
-        send_sqs_message(queue_url, json_response, sqs_message_group_id)
+        funk.save_data(bucket_name, file_name, json_response, queue_url, sqs_messageid_name)
+        
 
         logger.info("Successfully sent data to sqs")
 
         sqs = boto3.client("sqs", region_name="eu-west-2")
-        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+        if(receipt_handle):
+            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
         logger.info("Successfully deleted input data from sqs")
 
-        send_sns_message(arn, checkpoint)
+        funk.send_sns_message(checkpoint, arn, "Strata")
 
         logger.info("Successfully sent data to sns")
-    except NoDataInQueueError as e:
-        error_message = (
-            "There was no data in sqs queue in:  "
-            + current_module
-            + " |-  | Request ID: "
-            + str(context["aws_request_id"])
-        )
-        log_message = error_message + " | Line: " + str(e.__traceback__.tb_lineno)
+
     except AttributeError as e:
         error_message = (
             "Bad data encountered in "
@@ -169,54 +155,3 @@ def lambda_handler(event, context):
         else:
             logger.info("Successfully completed module: " + current_module)
             return {"success": True, "checkpoint": checkpoint}
-
-
-def send_sns_message(arn, checkpoint):
-    """
-    This function is responsible for sending notifications to the SNS Topic.
-    Notifications will be used to relay information to the BPM.
-
-    :param arn: The Address of the SNS topic - Type: String.
-    :param checkpoint: Location of process - Type: String.
-    :return:None.
-    """
-    sns = boto3.client("sns", region_name="eu-west-2")
-
-    sns_message = {
-        "success": True,
-        "module": "Strata",
-        "checkpoint": checkpoint,
-        "message": "Completed Strata",
-    }
-
-    sns.publish(TargetArn=arn, Message=json.dumps(sns_message))
-
-
-def send_sqs_message(queue_url, message, output_message_id):
-    """
-    This method is responsible for sending data to the SQS queue.
-    :param queue_url: The url of the SQS queue. - Type: String.
-    :param message: The message/data you wish to send to the SQS queue - Type: String.
-    :param output_message_id: The label of the record in the SQS queue - Type: String
-    :return: None
-    """
-    sqs = boto3.client('sqs', region_name='eu-west-2')
-
-    # MessageDeduplicationId is set to a random hash to overcome de-duplication,
-    # otherwise modules could not be re-run in the space of 5 Minutes.
-    sqs.send_message(
-        QueueUrl=queue_url,
-        MessageBody=message,
-        MessageGroupId=output_message_id,
-        MessageDeduplicationId=str(random.getrandbits(128)),
-    )
-
-
-def get_sqs_message(queue_url):
-    """
-    Retrieves message from the SQS queue.
-    :param queue_url: The url of the SQS queue. - Type: String.
-    :return: Message from queue - Type: String.
-    """
-    sqs = boto3.client("sqs", region_name="eu-west-2")
-    return sqs.receive_message(QueueUrl=queue_url)
