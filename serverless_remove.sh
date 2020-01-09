32fca356a1bd9fb90cab30b25bab46ebf588a78c
Kris Rogos <KrisRogos@users.noreply.github.com>
2020-01-09 11:34:48 +0000

    Update serverless_remove.sh

--- serverless_remove.sh
+++ serverless_remove.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 cd strata-repository
+echo Installing dependancies
+serverless plugin install --name serverless-latest-layer-version
 echo Destroying serverless bundle...
 serverless remove --verbose;
