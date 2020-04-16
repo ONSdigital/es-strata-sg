b5c7b79f0921db35902def57676bc95d765e63c2
Jordan Cooke <9076311+Jordancooke@users.noreply.github.com>
2020-04-16 14:06:14 +0100

    Update serverless_remove.sh

--- serverless_remove.sh
+++ serverless_remove.sh
@@ -6,3 +6,4 @@ serverless plugin install --name serverless-pseudo-parameters
 serverless plugin install --name serverless-latest-layer-version
 echo Destroying serverless bundle...
 serverless remove --verbose;
+
