c1a0d883bd26d72e59feaf301811e60ceb147860
Dom Ford <dom.ford@ons.gov.uk>
2020-09-21 16:31:04 +0100

    adding bpm notifications

--- tests/test_strata.py
+++ tests/test_strata.py
@@ -104,7 +104,6 @@ def test_incomplete_read_error():
                                                "IncompleteReadError")
 
 
-@mock.patch('strata_period_wrangler.aws_functions.send_bpm_status')
 @pytest.mark.parametrize(
     "which_lambda,which_environment_variables,expected_message,assertion",
     [
@@ -113,6 +112,7 @@ def test_incomplete_read_error():
         (lambda_wrangler_function, wrangler_environment_variables,
          "KeyError", test_generic_library.wrangler_assert)
     ])
+@mock.patch('strata_period_wrangler.aws_functions.send_bpm_status')
 def test_key_error(mock_send_status, which_lambda, which_environment_variables,
                    expected_message, assertion):
     test_generic_library.key_error(which_lambda, which_environment_variables,
